febb2b7f472d4ab4ef975036a586aa5a5ab64eca
mc-azakar <azakarmktg@gmail.com>
2025-04-21 14:08:41 +0800

    components

--- src/AppNavigation.jsx
+++ src/AppNavigation.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
-import Login from "../screens/Login.jsx"
+import Login from "../screens/Login.jsx";
+import Navbar from "../components/Navbar.jsx"
 
-function App() {
+function AppNavigation() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
-                {/* Add more routes like this: */}
+                <Route path="Navbar" element ={<Navbar/>}/>
                 {/* <Route path="/dashboard" element={<Dashboard />} /> */}
             </Routes>
         </Router>
     );
 }
 
-export default App;
+export default AppNavigation;
