085a60bb6fb3a6c8ed2bda971db1173073ba66ba
mc-azakar <azakarmktg@gmail.com>
2025-04-29 19:10:34 +0800

    message fix

--- screens/Queues.jsx
+++ screens/Queues.jsx
@@ -10,6 +10,7 @@ export default function Queues() {
   const [mobileSidebarOpen, setMobileSidebarOpen] = useState(false);
   const [openDropdown, setOpenDropdown] = useState(null);
   const dropdownRef = useRef(null);
+  const bottomRef = useRef(null);
 
   const [selectedCustomer, setSelectedCustomer] = useState(null);
 
@@ -17,23 +18,24 @@ export default function Queues() {
   const [inputMessage, setInputMessage] = useState("");
   const [isTyping, setIsTyping] = useState(false);
 
-
   const toggleDropdown = (name) => {
     setOpenDropdown((prev) => (prev === name ? null : name));
   };
   const sendMessage = () => {
     if (inputMessage.trim() === "") return; // Ignore empty messages
-  
+
     const newMessage = {
       sender: "user",
       content: inputMessage,
-      timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+      timestamp: new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
     };
-  
+
     setMessages((prev) => [...prev, newMessage]);
     setInputMessage(""); // Clear input
   };
-  
 
   const toggleSidebar = () => {
     setMobileSidebarOpen((prev) => !prev);
@@ -66,10 +68,8 @@ export default function Queues() {
   //Close dropdown when clicking outside
   useEffect(() => {
     const handleClickOutside = (event) => {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target)
-      ) { // Check if the click is outside the dropdown
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        // Check if the click is outside the dropdown
         setOpenDropdown(false);
       }
     };
@@ -79,6 +79,10 @@ export default function Queues() {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
 
   return (
     <div className="flex flex-col h-screen overflow-hidden">
@@ -128,10 +132,22 @@ export default function Queues() {
                         className="w-19 h-19 rounded-full object-cover"
                       />
                       <div>
-                        <p className={`text-sm font-medium ${selectedCustomer?.id === customer.id ? "text-[#6237A0]" : "text-gray-800"}`}>
+                        <p
+                          className={`text-sm font-medium ${
+                            selectedCustomer?.id === customer.id
+                              ? "text-[#6237A0]"
+                              : "text-gray-800"
+                          }`}
+                        >
                           {customer.name}
                         </p>
-                        <p className={`text-xs ${selectedCustomer?.id === customer.id ? "text-[#6237A0]" : "text-gray-500"}`}>
+                        <p
+                          className={`text-xs ${
+                            selectedCustomer?.id === customer.id
+                              ? "text-[#6237A0]"
+                              : "text-gray-500"
+                          }`}
+                        >
                           {customer.number}
                         </p>
                       </div>
@@ -170,101 +186,115 @@ export default function Queues() {
                         </h3>
                       </div>
                       <div className="relative ml-auto">
-                      <button className="p-2 text-black hover:text-[#6237A0] transition rounded-full"
-                      onClick={() => toggleDropdown('customerMenu')}>
-                        <MoreVertical size={22} />
-                      </button>
-                      {/* Dropdown menu */}
-      {openDropdown === 'customerMenu' && (
-        <div  ref={dropdownRef} className="absolute right-0 mt-2 w-44 bg-white border border-gray-300 rounded-md shadow-sm z-20">
-          <button
-            className="block w-full text-left px-4 py-2 text-sm text-red-500 hover:bg-gray-100"
-            onClick={() => {
-              console.log('End Chat clicked');
-              setOpenDropdown(null);
-            }}
-          >
-            End Chat
-          </button>
-          <div className="border-t border-gray-200" />
-          <button
-            className="block w-full text-left px-4 py-2 text-sm text-gray-800 hover:bg-gray-100"
-            onClick={() => {
-              console.log('Transfer Department clicked');
-              setOpenDropdown(null);
-            }}
-          >
-            Transfer Department
-          </button>
-        </div>
-      )}
-      </div>
+                        <button
+                          className="p-2 text-black hover:text-[#6237A0] transition rounded-full"
+                          onClick={() => toggleDropdown("customerMenu")}
+                        >
+                          <MoreVertical size={22} />
+                        </button>
+                        {/* Dropdown menu */}
+                        {openDropdown === "customerMenu" && (
+                          <div
+                            ref={dropdownRef}
+                            className="absolute right-0 mt-2 w-44 bg-white border border-gray-300 rounded-md shadow-sm z-20"
+                          >
+                            <button
+                              className="block w-full text-left px-4 py-2 text-sm text-red-500 hover:bg-gray-100"
+                              onClick={() => {
+                                console.log("End Chat clicked");
+                                setOpenDropdown(null);
+                              }}
+                            >
+                              End Chat
+                            </button>
+                            <div className="border-t border-gray-200" />
+                            <button
+                              className="block w-full text-left px-4 py-2 text-sm text-gray-800 hover:bg-gray-100"
+                              onClick={() => {
+                                console.log("Transfer Department clicked");
+                                setOpenDropdown(null);
+                              }}
+                            >
+                              Transfer Department
+                            </button>
+                          </div>
+                        )}
+                      </div>
                     </div>
                   </div>
                   {/* Chat messages */}
-                  <div className="flex-1 flex flex-col gap-4 overflow-y-auto overflow-x-hidden px-4 pb-2 auto-hide-scrollbar">
+                  <div className="flex-1 overflow-y-auto overflow-x-hidden px-4 pb-2 auto-hide-scrollbar">
+                    <div className="flex flex-col justify-end min-h-full gap-4 pt-4">
+                      
+                        <div className="flex items-end justify-end gap-2">
+                          <div className="text-sm text-gray-800 px-4 py-2 rounded-xl self-start max-w-[320px]">
+                            To connect you with the right support team, please
+                            select one of the following options:
+                          </div>
+                        </div>
                   
-                    <div className="flex items-end justify-end gap-2">
-                      {/* System message */}         
-                      <div className="text-sm text-gray-800 px-4 py-2 rounded-xl self-start max-w-[320px]">
-                        To connect you with the right support team, please
-                        select one of the following options:
-                      </div>
-                    </div>
 
-                    {/* Billing label bubble (left) */}
-                    <div className="flex flex-col items-start gap-1">
-                      <img
-                        src={selectedCustomer.profile}
-                        alt="avatar"
-                        className="w-8 h-8 rounded-full"
-                      />
-                      <div className="relative bg-[#6237A0] text-white px-4 py-2 ml-7 rounded-br-xl rounded-tr-xl rounded-bl-xl text-sm max-w-[300px]">
-                        Billing
-                        <div className="text-[10px] text-light text-gray-300 text-right mt-1 ml-2">
-                          1:20 PM
+                      {/* Billing label bubble (left) */}
+                      <div className="flex flex-col items-start gap-1">
+                        <img
+                          src={selectedCustomer.profile}
+                          alt="avatar"
+                          className="w-8 h-8 rounded-full"
+                        />
+                        <div className="relative bg-[#6237A0] text-white px-4 py-2 ml-7 rounded-br-xl rounded-tr-xl rounded-bl-xl text-sm max-w-[300px]">
+                          Billing
+                          <div className="text-[10px] text-light text-gray-300 text-right mt-1 ml-2">
+                            1:20 PM
+                          </div>
                         </div>
                       </div>
-                    </div>
 
-                    {/* System follow-up */}
-                    <div className="flex items-end justify-end gap-2">
-                      <div className="text-sm text-gray-800 px-4 py-2 rounded-xl self-start max-w-[320px]">
-                        We will be with you in a moment!
-                      </div>
-                    </div>
-
-                    {/* Divider */}
-                    <div className="text-[10px] text-gray-400 text-center flex items-center gap-2 my-2">
-                      <div className="flex-grow h-px bg-gray-200" />
-                      You are now chatting with Billing agent
-                      <div className="flex-grow h-px bg-gray-200" />
-                    </div>
-
-                    {/* Agent reply */}
-                    <div className="flex flex-col items-end gap-1 self-end">
-                      <img
-                        src="../src/assets/profile/av3.jpg"
-                        alt="agent"
-                        className="w-8 h-8 rounded-full"
-                      />
-                      <div className="relative bg-[#f5f5f5] px-3 py-2 rounded-bl-xl rounded-tl-xl rounded-br-xl text-sm max-w-[300px] mr-7">
-                        Hi, I’m Maria. How may I help you?
-                        <div className="text-[10px] text-right text-gray-400 mt-1">
-                          1:20 PM
+                      {/* System follow-up */}
+                      <div className="flex items-end justify-end gap-2">
+                        <div className="text-sm text-gray-800 px-4 py-2 rounded-xl self-start max-w-[320px]">
+                          We will be with you in a moment!
                         </div>
                       </div>
-                    </div>
-                    {messages.map((msg, index) => (
-                    <div key={index} className="flex items-end justify-end gap-2">
-                    <div className="bg-[#f5f5f5] text-gray-800 px-4 py-2 rounded-xl max-w-[320px] text-sm">
-                      {msg.content}
-                      <div className="text-[10px] text-right text-gray-400 mt-1">
-                        {msg.timestamp}
+
+                      {/* Divider */}
+                      <div className="text-[10px] text-gray-400 text-center flex items-center gap-2 my-2">
+                        <div className="flex-grow h-px bg-gray-200" />
+                        You are now chatting with Billing agent
+                        <div className="flex-grow h-px bg-gray-200" />
                       </div>
+
+                      {/* Agent reply */}
+                      <div className="flex flex-col items-end gap-1 self-end">
+                        <img
+                          src="../src/assets/profile/av3.jpg"
+                          alt="agent"
+                          className="w-8 h-8 rounded-full"
+                        />
+                        <div className="relative bg-[#f5f5f5] px-3 py-2 rounded-bl-xl rounded-tl-xl rounded-br-xl text-sm max-w-[300px] mr-7">
+                          Hi, I’m Maria. How may I help you?
+                          <div className="text-[10px] text-right text-gray-400 mt-1">
+                            1:20 PM
+                          </div>
+                        </div>
+                      </div>
+                      {/* Dynamic chat messages */}
+                      {messages.map((msg, index) => (
+                        <div
+                          key={index}
+                          className="flex items-end justify-end gap-2"
+                        >
+                          <div className="bg-[#f5f5f5] text-gray-800 px-4 py-2 rounded-xl max-w-[320px] text-sm">
+                            {msg.content}
+                            <div className="text-[10px] text-right text-gray-400 mt-1">
+                              {msg.timestamp}
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+
+                      {/* Auto scroll anchor */}
+                      <div ref={bottomRef} />
                     </div>
-                  </div>
-                  ))}
                   </div>
 
                   {/* Message input area*/}
@@ -278,13 +308,16 @@ export default function Queues() {
                       value={inputMessage}
                       onChange={(e) => setInputMessage(e.target.value)}
                       onKeyDown={(e) => {
-                        if (e.key === 'Enter') {
-                         sendMessage();
+                        if (e.key === "Enter") {
+                          sendMessage();
                         }
                       }}
                       className="flex-1 bg-gray-100 rounded-full px-4 py-2 focus:outline-none text-black"
                     />
-                    <button className="p-2 text-[#5C2E90]" onClick={sendMessage}>
+                    <button
+                      className="p-2 text-[#5C2E90]"
+                      onClick={sendMessage}
+                    >
                       <Send size={20} />
                     </button>
                   </div>
