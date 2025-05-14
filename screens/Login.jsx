25fb2695c3667781b426c28e194b0c7ac50937c6
servanacloud <servanacloud@gmail.com>
2025-05-14 14:20:36 +0800

    eye on eye off

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -46,45 +46,38 @@ export default function Login() {
               </div>
 
               <div className="space-y-2">
-                
-                <label
-                  htmlFor="password"
-                  className="block font-medium text-sm text-black"
-                >
-                  Password
-                  
-                </label>
-                <div className="relative">
-
-                <input
-                  id="password"
-                  type={showPassword ? "text" : "password"}
-                  value={password}
-                  placeholder="Enter your password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="w-full border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 pr-10"
-                  
-                />
-                {showPassword ? (
-                  <Eye className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 cursor-pointer"
-                  onClick={togglePasswordVisibility}
-                  />
-                ) : (
-                  <EyeOff className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 cursor-pointer"
-                  onClick={togglePasswordVisibility}
-                  />
-                )}
-                
-                </div>
-                <div className="text-right">
-                  <a
-                    href="#"
-                    className="text-sm text-[#6237A0] hover:underline"
-                  >
-                    Forgot password
-                  </a>
-                </div>
-              </div>
+  <label
+    htmlFor="password"
+    className="block font-medium text-sm text-black"
+  >
+    Password
+  </label>
+  <div className="relative">
+    <input
+      id="password"
+      type={showPassword ? "text" : "password"}
+      value={password}
+      placeholder="Enter your password"
+      onChange={(e) => setPassword(e.target.value)}
+      className="w-full border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 pr-10"
+    />
+    {password.length > 0 && (
+      showPassword ? (
+        <Eye size={18} strokeWidth={1}
+          className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 cursor-pointer"
+          onClick={togglePasswordVisibility}
+        />
+      ) : (
+        <EyeOff size={18} strokeWidth={1}
+          className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 cursor-pointer"
+          onClick={togglePasswordVisibility}
+        />
+      )
+    )}
+  </div>
+  <div className="text-right">
+  </div>
+</div>
 
               <button
                 className="w-full bg-[#6237A0] hover:bg-[#5C2E90] text-white font-semibold py-2 px-4 rounded-md transition"
