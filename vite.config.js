3f430865d80921bf8cd76be6f1eb1bcb387bff07
servanaApp <servana.app@gmail.com>
2025-05-17 13:04:45 +0800

    added port forwarding with ngrok

--- vite.config.js
+++ vite.config.js
@@ -9,3 +9,19 @@ export default defineConfig({
     open: true,
   },
 })
+
+
+// for port forwarding with ngrok
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: {
+//     host: true, // Allow access from network (required)
+//     port: 5173, // Ensure this matches the port ngrok is tunneling
+//     strictPort: true, // Prevent fallback to a random port
+//     allowedHosts: ['.ngrok-free.app'], // Allow any ngrok subdomain
+//   },
+// })
